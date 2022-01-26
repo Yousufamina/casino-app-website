53cc90b82caa892503026ce90ab4941d9dced021
aamna <aminayousuf_13@yahoo.com>
2022-01-26 23:55:07 +0500

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ class Header extends Component {
             <div>
                 <nav className="navbar navbar-light bg-light static-top">
                     <div className="container">
-                        <a className="navbar-brand" href="#!">Start Bootstrap</a>
-                        <a className="btn btn-primary" href="#signup">Sign Up</a>
+                        <a className="navbar-brand" href="#!">Online Casino</a>
+                        <a className="btn btn-primary" href="">Download</a>
                     </div>
                 </nav>
                 <header className="masthead">
